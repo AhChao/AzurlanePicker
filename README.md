eb7d286aa7eb96eaa2a58d933aabc76b07f8aaa5
YuLinChao <gash612@gmail.com>
2017-10-29 03:32:01 +0800

    [update]一些功能 -寫了一點點ReadMe -所有艦種目錄完成(可正常切換) -可以選擇分類區域 -日語化界面
    預計優先加入功能
    -新增分類
    -加入所選艦娘至分類

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # AzurlanePicker
+
+![Alt text](/img/background.jpg)
+
+**編輯相關**
+- 選擇你有的船艦來了解自己的收藏狀況
+- 可儲存自己的設定狀態
+- 可對船艦進行分類
+
+**考慮追加功能**
+- 更詳細的船艦資料
+- 儲存等級與突破 , 耗油
+
+**網頁展示**
+- https://ahchao.github.io/RainbowPuzzle/
--- src/azurlanePick.js
+++ src/azurlanePick.js
@@ -1,11 +1,29 @@
 var ownList = [];
-var ddList = [5,6,7,8,9,10,11,13,14,15,16,17,18,19,26,27,28,29,30,31,32,33,34,35,36,37,39,40,41,42,43,44,45,49,52,53,54,55,58,59,65,70,71,72,73,74,75,76,77,78,80,81,82,83,86,87,88,89,90,91,92,94,101,102,103,104,105,106,114,115,116,117,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,135,140,142,144,145,148,149,150,155,159,161,162,163,164,165,167,168,170,171,176,179,182,183,187,190,191,192,193,196,197,200,201,202,208,209,210,211,220,222,224,225,226,227,228,229,232,233,236,238,239,240,241,242,244,245,248,249,251,252,257,258,259,262,263,264,265,266,267,272,295,299,300,301,303,306];
+var ddList = [5,6,7,8,9,10,11,13,14,15,16,17,18,19,26,27,28,29,81,82,83,86,87,88,89,90,91,92,94,101,102,103,104,105,106,114,115,116,117,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,135,140,142,144,145,148,149,150,155,159,161,162,163,164,165,167,168,170,171,176,179,182,183,187,190,191,192,193,196,197,200,201,202,208,209,210,211,220,222,224,225,226,227,228,229,232,233,236,238,239,240,241,242,244,245,248,249,251,252,257,258,259,262,263,264,265,266,267,272,295,299,300,301,303,306];
+var clList = [29,30,31,32,33,34,35,36,37,104,105,106,114,115,116,117,179,182,183,187,238,239,240,241,257,258,259,262,303]
+var caList = [39,40,41,42,43,44,45,49,119,120,121,122,123,124,125,126,190,191,192,193,196,197,200,201,202,242,244,245]
+var bcList = [127,128,129,248,249]
+var bbList = [52,53,54,55,58,59,65,130,131,132,133,135,208,209,210,211,251]
+var cvlList= [70,71,72,75,140,142,220,222]
+var cvList = [73,74,76,77,78,144,145,148,224,225,226,227,228,229,252]
+var bmList = [149,150]
+var arList = [80,232]
+var allList = ddList.concat(clList,caList,bcList,bbList,cvlList,cvList,bmList,arList);
 
 function init()
 {
 	for(var i=0;i<307;i++) ownList[i]=0;
+	LoadSelectPicture();
+	/*//bubble sort
+	var i=0,j=0,temp;
+	for (i = 0; i < allList.length - 1; i++)
+		for (j = 0; j < allList.length - 1 - i; j++)
+			if (allList[j] > allList[j + 1]) {
+				temp = allList[j];
+				allList[j] = allList[j + 1];
+				allList[j + 1] = temp;
+			}*/
 }
-
 function ChangeBGColor(line,color)
 {
 	var bgId = "#"+line+"BG";
@@ -29,6 +47,8 @@ function ChangeBGColor(line,color)
 	.attr("xlink:href", "./img/001.jpg")
 	.attr("border-radius",10)
 	.attr("clip-path","url(#clip)");
+	/*
+	//顯示等級用
 	group.append("svg:rect")
 	.attr('x', x)
 	.attr('y', parseInt(y)+height )
@@ -42,7 +62,7 @@ function ChangeBGColor(line,color)
 	.attr('x', x+25)
 	.attr('y', parseInt(y)+parseInt(height)+20)
 	.attr('color','#FFFFFF')
-	.text("LV 100");		
+	.text("LV 100");*/		
 }
 function RemoveElement(element)
 {
@@ -69,9 +89,10 @@ function OwnOrNot(element)
 	updateOwnText(ownList);
 }
 function LoadSelectPicture()
-{	
+{		
 	var svgId = "#selectViewSVG";
 	var svg = d3.select(svgId);	
+	clearSVGContent("selectViewSVG");
 	var background = d3.select("#selectViewBG");
 	var svgHeight = d3.select(svgId).attr("height");
 
@@ -83,26 +104,24 @@ function LoadSelectPicture()
 	var y = initY;
 	
 	var selectionTag = document.getElementById("filterTag");
-	console.log(selectionTag.value);
 	var chosenList;
-	if(selectionTag.value == "alll")
-	{			
-		//doSomeThing		
-	}
-	else if(selectionTag.value == "dd")
-	{
-		chosenList=ddList;
-	}
-	else
-	{
-		chosenList="errorList"
-	}
+	if(selectionTag.value == "all") chosenList=allList;
+	else if(selectionTag.value == "dd")	chosenList=ddList;
+	else if(selectionTag.value == "cl")	chosenList=clList;
+	else if(selectionTag.value == "ca")	chosenList=caList;
+	else if(selectionTag.value == "bc")	chosenList=bcList;
+	else if(selectionTag.value == "bb")	chosenList=bbList;
+	else if(selectionTag.value == "cvl")chosenList=cvlList;
+	else if(selectionTag.value == "cv")	chosenList=cvList;
+	else if(selectionTag.value == "bm")	chosenList=bmList;
+	else if(selectionTag.value == "ar")	chosenList=arList;
+	else	chosenList="errorList"
+
 	if(chosenList!="errorList")
 	{
 		x = initX;
 		y = initY;
 		var i=0;
-		console.log(chosenList.length);
 		while(i<chosenList.length)
 		{
 			var number = chosenList[i];
@@ -112,8 +131,8 @@ function LoadSelectPicture()
 			picLink=picLink+number+".jpg";
 
 			var op;
-			if( ownList[number]>0) op=1;
-			else op=0.3
+			if( ownList[parseInt(number)]>0) op=1;
+			else op=0.3;
 
 			var group = svg.append("g");
 			group.append("svg:image")
@@ -163,13 +182,14 @@ var loadJSONFile = function(event) {
 	        }
 	        //刷新當前畫面選單
 	        updateOwnText(ownList);
+	        LoadSelectPicture();
         };
         reader.readAsText(input.files[0]);        
     };
 
 function updateOwnText( targetList )
 {
-	var total = targetList.length-1;
+	var total = allList.length;
 	var own = 0 ;
 	for(var i=0;i<targetList.length;i++)
 	{
@@ -183,4 +203,44 @@ function updateOwnText( targetList )
 	ownText.innerText="【持有數: " +own +" / "+ total +"】【持有率: "+ ownPercent + " %】"
 }
 
+function clearSVGContent(svgId)
+{
+	var targetSVG = document.getElementById(svgId);
+	var allChild = targetSVG.children;
+
+	while(allChild.length>1)
+	{
+		for(var i in allChild)
+		{
+			if(allChild[i].id!="selectViewBG") RemoveElement(allChild[i]);
+		}
+	}
+	var idString = "#"+svgId;
+	var svg = d3.select(idString);
+	var svgBG = d3.select(selectViewBG);
+	svg.attr("height","150")
+	   .attr("width","1000");
+	svgBG.attr("height","150")
+	   .attr("width","1000");
+}
+function addCharacterMode(bgId)
+{
+	var BGDom = document.getElementById(bgId);
+	bgId = "#"+bgId;
+	var BG = d3.select(bgId);
+	var width = BG.attr("width")*0.95;
+	var height = BG.attr("height")*0.8;
+	var x = BG.attr("width")*0.025;
+	var y = BG.attr("height")*0.1;
+
+	d3.select(bgId).append("svg:rect")
+	.attr("width",width)
+	.attr("height",height)
+	.attr("x",x)
+	.attr("y",y)
+	.attr("fill","None")
+	.attr("stroke","red")
+	.attr("stroke-width","10")
+	.attr("id","selectRect");
+}
 init();
